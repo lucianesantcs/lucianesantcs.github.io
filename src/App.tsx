4f6c47dea78efa8da2ec294413b0073689abd228
Luciane Santos <lucianesantosx2@gmail.com>
2024-10-27 18:42:35 -0300

    feat: criado section sobre

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Footer from "./components/layouts/Footer/Footer";
 import Header from "./components/layouts/Header/Header";
+import Sobre from "./sections/Sobre";
 
 function App() {
   return (
     <>
       <Header />
-      <main className="container min-h-dvh px-3 md:px-10 lg:px-20"></main>
+      <main className="container min-h-dvh px-3 md:px-10 lg:px-20">
+        <Sobre />
+      </main>
       <Footer />
     </>
   );
---
+++ src/sections/Sobre.tsx
@@ -0,0 +1,29 @@
+import SocialIcons from "@/components/ui/SocialIcons";
+
+const Sobre = () => {
+  return (
+    <section className="flex">
+      <section className="flex w-[32.75rem] mt-28 flex-col gap-8">
+        <div>
+          <h1 className="font-serif text-4xl font-medium">Luciane Santos</h1>
+          <h2 className="font-serif text-xl font-medium">Desenvolvedora Front-End</h2>
+          <h2 className="font-serif text-base font-semibold">& UI Designer</h2>
+        </div>
+        <SocialIcons showLabel={true} position="vertical" />
+      </section>
+
+      <section className="flex w-[33.6875rem] mt-28 flex-col gap-4">
+        <h2 className="font-serif text-2xl font-medium">Sobre</h2>
+        <p className="font-sans text-base">
+          Profissional Front-End com mais de três anos de experiência em
+          desenvolvimento web dominando as linguagens HTML, CSS/Sass, JavaScript
+          e TypeScript. Criação de layouts utilizando o framework Angular e
+          biblioteca ReactJS. Conhecimento de User Interface/User Experience
+          para o design de interfaces utilizando a ferramenta Figma.
+        </p>
+      </section>
+    </section>
+  );
+};
+
+export default Sobre;
