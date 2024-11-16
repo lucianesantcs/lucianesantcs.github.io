b9c55eb90c62fbca3f1aacbd0ed66c9e81b433fa
Luciane Santos <lucianesantosx2@gmail.com>
2024-11-15 22:33:05 -0300

    feat: criado componente link

---
+++ src/components/ui/Link.tsx
@@ -0,0 +1,31 @@
+import React, { ReactNode } from "react";
+
+type LinkProps = {
+  label?: string;
+  showLabel?: boolean;
+  url: string;
+  className?: string;
+  children?: ReactNode;
+};
+
+const Link = ({
+  label,
+  showLabel = true,
+  className,
+  url,
+  children,
+}: LinkProps) => {
+  const childrenArray = React.Children?.toArray(children);
+  const baseStyles = "flex gap-3 font-serif text-xs font-semibold";
+  const linkStyles = `${baseStyles} ${className}`.trim();
+
+  return (
+    <a href={url} title={label} target="_blank" className={linkStyles} referrerPolicy="no-referrer">
+      {childrenArray?.length > 1 ? childrenArray[0] : ""}
+      {showLabel && label}
+      {childrenArray?.length > 1 ? childrenArray[1] : childrenArray[0]}
+    </a>
+  );
+};
+
+export default Link;
