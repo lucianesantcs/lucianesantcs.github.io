f4aafe7a8b2e47fb2789b02d3b7dfd84fb09a0d3
Luciane Santos <lucianesantcs@gmail.com>
2025-02-18 22:03:34 -0300

    feat: melhoria no footer

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -1,25 +1,11 @@
 import { ChevronUp } from "lucide-react";
-import assinatura from "@/assets/images/svg/assinatura.svg";
-import SocialIcons from "@/components/ui/SocialIcons";
-import { socialIcons } from "@/shared/constants";
 
 const Footer = () => {
   return (
     <footer className="max-w-144 mx-auto flex w-full justify-between px-3 py-10 md:px-10 lg:px-20 md:gap-28 lg:gap-40 mt-16">
-      <div className="flex gap-3">
-        {socialIcons.map((socialIcon) => (
-          <SocialIcons
-            key={socialIcon.label}
-            label={socialIcon.label}
-            iconName={socialIcon.iconName}
-            url={socialIcon.url}
-            position="vertical"
-            showExternalLinkIcon={false}
-            showLabel={false}
-          />
-        ))}
-      </div>
-      <img src={assinatura} />
+      <span className="font-serif">
+      © 2025 <strong>Luciane Santos</strong>
+      </span>
       <ChevronUp color="#e5e5e5" size={16} />
     </footer>
   );
