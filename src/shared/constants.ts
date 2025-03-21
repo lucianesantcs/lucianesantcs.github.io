21789d9f267535c408e41f42a027e96201a2a84d
Luciane Santos <lucianesantcs@gmail.com>
2025-03-21 15:49:32 -0300

    fix: correção caminho curriculo

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -4,7 +4,7 @@ export const socialIcons: SocialIconsProps[] = [
     {
       label: "Download CV",
       iconName: "Download",
-      url: "curriculo-lucianesantos.pdf",
+      url: "./curriculo-lucianesantos.pdf",
     },
     {
       label: "Github",
